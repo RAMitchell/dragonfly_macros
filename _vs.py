590370c5675310dbcb9eb137316a6d00856331f4
Rory Mitchell <rorym@nvidia.com>
2022-06-07 13:45:45 +0200

    vscode

--- _vs.py
+++ _vs.py
@@ -6,20 +6,13 @@ class  GlobalVSMappings(MappingRule):
     mapping = {
         #ide shortcuts
 	"run": Key("c-f5"),
-	"build": Key("f7"),
+	"build": Key("ctrl:down,shift:down/1,b/1,shift:up,ctrl:up"),
 	"device vector": Text("thrust::device_vector<"),
 	"thrust": Text("thrust::"),
-	"(<n>) next tab": Key("ctrl:down,alt:down/2,pgdown/2,ctrl:up,alt:up"),
-	"reformat": Key("c-r") + Key("c-f"),
+	"reformat":Key("alt:down,shift:down/1,k/1,shift:up,alt:up"),
 	"code": Key("ctrl:down,alt:down/1,0/1,alt:up,ctrl:up"),
 	"output": Key("a-2"),
 	"rename": Key("c-r,r/1"),
-	"blast": Key("c-r,r/50,end/50,underscore/50,enter/50"),
-	"bop": Key("c-pgdown"),
-	"doc": Key("ctrl:down, shift:down/2, d/2, ctrl:up, shift:up"),
-	"comment": Key("c-k, c-c, escape"),
-	"uncomment": Key("c-k, c-u, escape"),
-	"camel": Key("ctrl:down,shift:down/1,k/1,shift:up,ctrl:up,ctrl:down,shift:down/1,c/1,shift:up,ctrl:up"),
 
         }
     extras = [
